d1768eb1dfe4138049e0bbca741efbba4993078c
HerdiansyahMaulanAhmad <herdiansyahmaulan8792gmail.com>
2025-07-09 19:02:17 +0700

    update proyek, redirect

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <div className="mt-32 py-4 flex md:flex-row flex-col gap-6 md:gap-0 justify-between items-center">
         <h1 className="text-2xl font-bold">Portofolio</h1> 
         <div className="flex gap-7">
-            <a href="beranda">Beranda</a>
+            <a href="#beranda">Beranda</a>
             <a href="#tentang">Tentang</a>
             <a href="#proyek">Proyek</a>
         </div>
@@ -11,13 +11,13 @@ const Footer = () => {
             <a href="https://github.com/HerdiansyahMaulanAhmad">
                 <i className="ri-github-fill ri-2x"></i>
             </a>
-            <a href="#">
+            <a href="https://www.instagram.com/hrdiansyhma_?igsh=aHl3Y3R0ZGg2dnZx&utm_source=qr">
                 <i className="ri-instagram-fill ri-2x"></i>
             </a>
-            <a href="#">
+            <a href="https://www.linkedin.com/in/herdiansyah-maulan-ahmad-810153372">
                 <i className="ri-linkedin-fill ri-2x"></i>
             </a>
-            <a href="#">
+            <a href="https://www.tiktok.com/@xxynzyy_?_t=ZS-8xsb2K1qglN&_r=1">
                 <i className="ri-tiktok-fill ri-2x"></i>
             </a>
         </div>
--- src/data.js
+++ src/data.js
@@ -11,12 +11,13 @@ import Tools2 from "/assets/tools/github.png";
 import Tools3 from "/assets/tools/laragon.png";
 import Tools4 from "/assets/tools/mysql.png";
 import Tools5 from "/assets/tools/html.png";
-import Tools6 from "/assets/tools/bootstrap.png";
+import Tools6 from "/assets/tools/css.png";
 import Tools7 from "/assets/tools/js.png";
-import Tools8 from "/assets/tools/tailwind.png";
-import Tools9 from "/assets/tools/reactjs.png";
-import Tools10 from "/assets/tools/laravel.png";
-import Tools11 from "/assets/tools/canva.png";
+import Tools8 from "/assets/tools/php.png";
+import Tools9 from "/assets/tools/tailwind.png";
+import Tools10 from "/assets/tools/reactjs.png";
+import Tools11 from "/assets/tools/laravel.png";
+import Tools12 from "/assets/tools/canva.png";
 
 export const listTools = [
   {
@@ -71,87 +72,103 @@ export const listTools = [
   {
     id: 8,
     gambar: Tools8,
-    nama: "Tailwind CSS",
-    ket: "Framework",
+    nama: "PHP",
+    ket: "Language",
     dad: "400",
   },
   {
     id: 9,
     gambar: Tools9,
+    nama: "Tailwind CSS",
+    ket: "Framework",
+    dad: "400",
+  },
+  {
+    id: 10,
+    gambar: Tools10,
     nama: "React JS",
     ket: "Framework",
     dad: "800",
   },
   {
-    id: 10,
-    gambar: Tools10,
+    id: 11,
+    gambar: Tools11,
     nama: "Laravel",
     ket: "Framework",
     dad: "900",
   },
   {
-    id: 11,
-    gambar: Tools11,
+    id: 12,
+    gambar: Tools12,
     nama: "Canva",
     ket: "Design",
     dad: "1000",
   },
 ];
 
-import Proyek1 from "/assets/proyek/proyek1.webp";
-import Proyek2 from "/assets/proyek/proyek2.webp";
-import Proyek3 from "/assets/proyek/proyek3.webp";
-import Proyek4 from "/assets/proyek/proyek4.webp";
-import Proyek5 from "/assets/proyek/proyek5.webp";
-import Proyek6 from "/assets/proyek/proyek6.webp";
+import Proyek1 from "/assets/proyek/projek1.jpg";
+import Proyek2 from "/assets/proyek/projek3.jpg";
+import Proyek3 from "/assets/proyek/projek2.jpg";
+import Proyek4 from "/assets/proyek/projek4.jpg";
+// import Proyek4 from "/assets/proyek/proyek4.webp";
+// import Proyek5 from "/assets/proyek/proyek5.webp";
+// import Proyek6 from "/assets/proyek/proyek6.webp";
 
 export const listProyek = [
   {
     id: 1,
     gambar: Proyek1,
-    nama: "Website Sekolah",
-    desk: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, laborum!",
-    tools: ["HTML", "CSS", "Javascript", "AOS"],
+    nama: "Website Cuaca",
+    desk: "Pantau kondisi cuaca global secara real-time melalui website ini.",
+    tools: ["HTML", "CSS", "Javascript", "API"],
     dad: "200",
   },
   {
     id: 2,
     gambar: Proyek2,
-    nama: "Company Profile",
-    desk: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, laborum!",
-    tools: ["HTML", "CSS", "Javascript", "AOS", "Swiper", "Lightbox Gallery"],
+    nama: "Website Klinik Kesehatan",
+    desk: "Solusi kesehatan terpadu dalam satu platform digital yang mudah diakses.",
+    tools: ["Laravel", "Bootstrap", "PHP", "Javascript"],
     dad: "300",
   },
   {
     id: 3,
     gambar: Proyek3,
-    nama: "Web Pernikahan 2.0",
-    desk: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, laborum!",
-    tools: ["Vite", "ReactJS", "TailwindCSS", "AOS"],
+    nama: "Website Aspirasi",
+    desk: "Platform digital untuk menyampaikan aspirasi secara terbuka dan transparan.",
+    tools: ["Laravel", "Bootstrap", "PHP", "CSS", "Javascript"],
     dad: "400",
   },
   {
     id: 4,
     gambar: Proyek4,
-    nama: "Website Course",
-    desk: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, laborum!",
-    tools: ["Vite", "ReactJS", "Bootstrap", "AOS"],
-    dad: "500",
+    nama: "Website Portofolio",
+    desk: "Portofolio digital yang merepresentasikan kompetensi dan dedikasi saya.",
+    tools: ["Javascript", "Vite", "ReactJS", "Tailwind CSS"],
+    dad: "300",
   },
-  {
-    id: 5,
-    gambar: Proyek5,
-    nama: "Web Portfolio",
-    desk: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, laborum!",
-    tools: ["HTML", "CSS", "Javascript", "Bootsrap"],
-    dad: "600",
-  },
-  {
-    id: 6,
-    gambar: Proyek6,
-    nama: "Company Profile 2.0",
-    desk: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, laborum!",
-    tools: ["NextJS", "TailwindCSS", "Framermotion"],
-    dad: "700",
-  },
+  // {
+  //   id: 4,
+  //   gambar: Proyek4,
+  //   nama: "Website Course",
+  //   desk: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, laborum!",
+  //   tools: ["Vite", "ReactJS", "Bootstrap", "AOS"],
+  //   dad: "500",
+  // },
+  // {
+  //   id: 5,
+  //   gambar: Proyek5,
+  //   nama: "Web Portfolio",
+  //   desk: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, laborum!",
+  //   tools: ["HTML", "CSS", "Javascript", "Bootsrap"],
+  //   dad: "600",
+  // },
+  // {
+  //   id: 6,
+  //   gambar: Proyek6,
+  //   nama: "Company Profile 2.0",
+  //   desk: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, laborum!",
+  //   tools: ["NextJS", "TailwindCSS", "Framermotion"],
+  //   dad: "700",
+  // },
 ];
